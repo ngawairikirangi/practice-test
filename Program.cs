c0b90785c8c457e0d33a9cd2b1243bb49095c050
Ngawai Rikirangi <9978212@BCS.NET.NZ>
2017-08-11 12:30:06 +1200

    this is a test

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter you first name");
+            var fname = Console.ReadLine();
+            
+
+            Console.WriteLine("Please enter you last name");
+            var lname = Console.ReadLine();
+            
+            Console.WriteLine(fname);
+            Console.WriteLine(lname);
+
+        }
+    }
+}
